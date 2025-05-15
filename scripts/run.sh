9011d1b4e742b8012970852593512fef9b9087e4
Jari <kolehmainen@Jaris-MacBook-Pro.local>
2025-05-15 09:57:37 -0700

    Fixing the folder name.

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,6 +9,6 @@ torchrun \
     --rdzv-id=dipaco.run \
     --rdzv-backend=c10d \
     --rdzv-endpoint="$MASTER_ADDR:$MASTER_PORT" \
-    "$ROOT/src/noloco/train.py" \
+    "$ROOT/src/dipaco/train.py" \
     --config-path "$ROOT/configs/C4" \
     --config-name c4_dipaco_7b_64.yaml
